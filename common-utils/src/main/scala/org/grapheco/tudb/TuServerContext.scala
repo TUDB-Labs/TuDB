533bda05ca486f2ee2008cf7309027156b118556
Johnny <yangke1110@gmail.com>
2022-06-14 11:13:47 +0800

    Supplement commits  from 0606 to 0610

---
+++ common-utils/src/main/scala/org/grapheco/tudb/TuServerContext.scala
@@ -0,0 +1,23 @@
+package org.grapheco.tudb
+
+/** @Author: Airzihao
+  * @Description:
+  * @Date: Created at 17:50 2022/4/12
+  * @Modified By:
+  */
+object TuServerContext extends ContextMap {
+
+  def setDataPath(path: String): Unit = {
+    // Todo: check whether the path is illegal.
+    // for a new instance, the path should be empty
+    // for an old instance, the path should include a description file.
+    super.put("dataPath", path)
+  }
+
+  def getDataPath: String = super.get[String]("dataPath")
+
+  def setPort(port: Int): Unit = super.put("bindPort", port)
+
+  def getPort: Int = super.get[Int]("bindPort")
+
+}
