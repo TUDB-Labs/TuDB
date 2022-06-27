1d838814f2f398443229b50badaf89208e040d61
livehl <livehl@126.com>
2022-06-27 13:51:52 +0800

    1. add memory index engine (#37)
    * 1. add memory index engine
    
    * 1. add test unit
    
    * 1. add test unit
    
    Co-authored-by: huanglin <huang.lin@tudb.a>

--- storage/src/main/scala/org/grapheco/tudb/store/index/IndexFactory.scala
+++ storage/src/main/scala/org/grapheco/tudb/store/index/IndexFactory.scala
@@ -6,30 +6,32 @@ import com.typesafe.scalalogging.StrictLogging
 import java.net.URI
 
 /** @author: huanglin
-  * @createDate: 2022-06-20 17:19:08
-  * @description: this is the index engine create  factory.
-  */
+ * @createDate: 2022-06-20 17:19:08
+ * @description: this is the index engine create  factory.
+ */
 object IndexFactory extends StrictLogging {
 
   /** create index engine by uri
-    * index engine url
-    * memory://{any}  use hashmap storage index data
-    * es://ip:port   use  es (Elasticsearch) storage index data,ip:port is es service address
-    *  db://{path}  use rocksdb storage index data ,path is rocksdb data storage location
-    *  empty is empty implement ,  use this engine where no  index is used
-    * @param indexUri
-    * @return index engine
-    */
+   * index engine url
+   * memory://{any}  use hashmap storage index data
+   * es://ip:port   use  es (Elasticsearch) storage index data,ip:port is es service address
+   * db://{path}  use rocksdb storage index data ,path is rocksdb data storage location
+   * empty is empty implement ,  use this engine where no  index is used
+   *
+   * @param indexUri
+   * @return index engine
+   */
   def newIndex(indexUri: String): IndexServer = {
-    if (null == indexUri || indexUri.isEmpty || !indexUri.contains("://")) {
+    if (null == indexUri || indexUri.isEmpty || !indexUri.contains("://") || !indexUri.startsWith("tudb")) {
       new EmptyIndexServerImpl(Map.empty)
     } else {
-      val url=new URI(indexUri)
-      val params = url.getQuery.split("&").map(v=>v.split("=").toList).map(v=> v(0)-> ( if (v.size>1) v(1) else "" ) ).toMap
-      params.getOrElse("type","dummy") match {
-//        case "hashmap" => new MemoryIndexServerImpl(indexValue)
-//        case "db"      => new RocksIndexServerImpl(indexValue)
-        case _         => new EmptyIndexServerImpl(params)
+      val url = new URI(indexUri)
+      val params = if (url.getQuery == null) Map.empty[String, String] else url.getQuery.split("&")
+        .map(v => v.split("=").toList).map(v => v(0) -> (if (v.size > 1) v(1) else "")).toMap
+      params.getOrElse("type", "dummy") match {
+        case "memory" => new MemoryIndexServerImpl(params)
+        //        case "db"      => new RocksIndexServerImpl(indexValue)
+        case _ => new EmptyIndexServerImpl(params)
       }
     }
   }
---
+++ storage/src/test/scala/org/grapheco/tudb/test/index/IndexServerTest.scala
@@ -0,0 +1,58 @@
+package org.grapheco.tudb.test.index
+
+import org.grapheco.tudb.store.index.{EmptyIndexServerImpl, IndexFactory, IndexServer, MemoryIndexServerImpl}
+import org.junit._
+import org.junit.runners.MethodSorters
+
+
+@FixMethodOrder(MethodSorters.NAME_ASCENDING)
+class IndexServerTest {
+
+  @Test
+  def testCreateEngine(): Unit = {
+    //empty index server
+    val dummyIndexImpl = IndexFactory.newIndex("tudb://index?type=dummy")
+    Assert.assertTrue(dummyIndexImpl.isInstanceOf[EmptyIndexServerImpl])
+    val errorIndexImpl = IndexFactory.newIndex("asdfasdfasdf")
+    Assert.assertTrue(errorIndexImpl.isInstanceOf[EmptyIndexServerImpl])
+    val error2IndexImpl = IndexFactory.newIndex("tudb://index?type=asdfasdfasdf")
+    Assert.assertTrue(error2IndexImpl.isInstanceOf[EmptyIndexServerImpl])
+    val error3IndexImpl = IndexFactory.newIndex("https://www.google.com")
+    Assert.assertTrue(error3IndexImpl.isInstanceOf[EmptyIndexServerImpl])
+    val error4IndexImpl = IndexFactory.newIndex("tudb://www.google.com")
+    Assert.assertTrue(error4IndexImpl.isInstanceOf[EmptyIndexServerImpl])
+    val error5IndexImpl = IndexFactory.newIndex("asdfsdf://www.google.com")
+    Assert.assertTrue(error5IndexImpl.isInstanceOf[EmptyIndexServerImpl])
+    //memory index server
+    val memoryIndexImpl = IndexFactory.newIndex("tudb://index?type=memory")
+    Assert.assertTrue(memoryIndexImpl.isInstanceOf[MemoryIndexServerImpl])
+  }
+
+  @Test
+  def testMemoryEngine(): Unit = {
+    val memoryIndexImpl = IndexFactory.newIndex("tudb://index?type=memory")
+    memoryIndexImpl.init(Map.empty)
+    testEngineImpl(memoryIndexImpl)
+  }
+
+  /*
+  test index engine impl
+   */
+  def testEngineImpl(impl: IndexServer): Unit = {
+    val (propertyKey,nodeId)=("property_key_value",1)
+    impl.addIndex(propertyKey, nodeId)
+    Assert.assertTrue(impl.getIndexByKey(propertyKey).contains(nodeId))
+    Assert.assertFalse(impl.getIndexByKey(propertyKey).contains(2))
+    impl.removeIndex(propertyKey, nodeId)
+    Assert.assertFalse(impl.getIndexByKey(propertyKey).contains(nodeId))
+    Assert.assertTrue(impl.getIndexByKey(propertyKey).isEmpty)
+  }
+
+
+  @Before
+  def cleanUp(): Unit = {
+
+  }
+
+
+}
