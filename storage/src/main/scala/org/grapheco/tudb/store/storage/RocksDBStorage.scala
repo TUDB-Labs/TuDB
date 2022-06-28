698704a44569ebcbceee28b31b1d7208b2590cb4
huanglin <huang.lin@tudb.a>
2022-06-28 09:26:22 +0800

    1. change rocksdb config  to single class 2. add high performance option

--- storage/src/main/scala/org/grapheco/tudb/store/storage/RocksDBStorage.scala
+++ storage/src/main/scala/org/grapheco/tudb/store/storage/RocksDBStorage.scala
@@ -30,49 +30,19 @@ object RocksDBStorage extends LazyLogging {
 
     if (rocksdbConfigPath == "default") {
       logger.debug("use default setting")
-      val options: Options = new Options()
-      val tableConfig = new BlockBasedTableConfig()
-
-      tableConfig
-        .setFilterPolicy(new BloomFilter(15, false))
-        .setBlockSize(16L * 1024L)
-        .setBlockCache(new LRUCache(1024L * 1024L * 1024L))
-
-      options
-        .setTableFormatConfig(tableConfig)
-        .setCreateIfMissing(createIfMissing)
-        .setCompressionType(CompressionType.LZ4_COMPRESSION)
-        .setCompactionStyle(CompactionStyle.LEVEL)
-        .setDisableAutoCompactions(
-          false
-        ) // true will invalid the compaction trigger, maybe
-        .setOptimizeFiltersForHits(
-          false
-        ) // true will not generate BloomFilter for L0.
-        .setMaxBackgroundJobs(4)
-        .setMaxWriteBufferNumber(8)
-        .setSkipCheckingSstFileSizesOnDbOpen(true)
-        .setLevelCompactionDynamicLevelBytes(true)
-        .setAllowConcurrentMemtableWrite(true)
-        .setCompactionReadaheadSize(2 * 1024 * 1024L)
-        .setMaxOpenFiles(-1)
-        .setUseDirectIoForFlushAndCompaction(true) // maybe importer use
-        .setWriteBufferSize(256L * 1024L * 1024L)
-        .setMinWriteBufferNumberToMerge(2) // 2 or 3 better performance
-        .setLevel0FileNumCompactionTrigger(10) // level0 file num = 10, compression l0->l1 start.(invalid, why???);level0 size=256M * 3 * 10 = 7G
-        .setLevel0SlowdownWritesTrigger(20)
-        .setLevel0StopWritesTrigger(40)
-        .setMaxBytesForLevelBase(
-          256L * 1024L * 1024L * 2 * 10L
-        ) // total size of level1(same as level0)
-        .setMaxBytesForLevelMultiplier(10)
-        .setTargetFileSizeBase(
-          256L * 1024L * 1024L
-        ) // maxBytesForLevelBase / 10 or 15
-        .setTargetFileSizeMultiplier(2)
 
       try {
-        new RocksDBStorage(RocksDB.open(options, path))
+        new RocksDBStorage(RocksDB.open(RocksDBStorageConfig.getDefault(createIfMissing), path))
+      } catch {
+        case ex: Exception =>
+          throw new TuDBException(s"$path, ${ex.getMessage}")
+      }
+
+    } else if (rocksdbConfigPath == "performance") {
+      logger.debug("use performance setting")
+
+      try {
+        new RocksDBStorage(RocksDB.open(RocksDBStorageConfig.getPerformance(createIfMissing), path))
       } catch {
         case ex: Exception =>
           throw new TuDBException(s"$path, ${ex.getMessage}")
