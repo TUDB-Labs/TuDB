80ca65674eca889cfaddd3f7ab17db2c5064a8ee
huanglin <huang.lin@tudb.a>
2022-06-29 18:31:00 +0800

    1.add json support

---
+++ server/src/main/scala/org/grapheco/tudb/TuDBJsonTool.scala
@@ -0,0 +1,84 @@
+/** Copyright (c) 2022 PandaDB * */
+package org.grapheco.tudb
+
+import com.fasterxml.jackson.databind.{ObjectMapper, SerializationFeature}
+import org.grapheco.lynx.PathTriple
+import org.grapheco.lynx.types.property.LynxPath
+import org.grapheco.tudb.graph.{TuNode, TuRelationship}
+
+import java.text.SimpleDateFormat
+
+/** add toJson method to AnyRef
+  *
+  * use case:
+  *
+  * import org.grapheco.tudb.TuDB.TuDBJsonTool.AnyRefAddMethod
+  *
+  * val mapString=Map("a"->1,"b"->2).toJson()
+  * val nodeString=Node("a").toJson()
+  * AnyObject.toJson()
+  */
+object TuDBJsonTool {
+  val objectMapper = new ObjectMapper()
+    .findAndRegisterModules()
+    .configure(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS, false)
+    // 持续时间序列化为字符串
+    .configure(SerializationFeature.WRITE_DURATIONS_AS_TIMESTAMPS, false)
+    // 当出现 Java 类中未知的属性时不报错，而是忽略此 JSON 字段
+    .configure(SerializationFeature.FAIL_ON_UNWRAPPED_TYPE_IDENTIFIERS, false)
+    // 枚举类型调用 `toString` 方法进行序列化
+    .configure(SerializationFeature.WRITE_ENUMS_USING_TO_STRING, true)
+    // 设置 java.util.Date 类型序列化格式
+    .setDateFormat(new SimpleDateFormat("yyyy-MM-dd HH:mm:ss"))
+
+  implicit class AnyRefAddMethod[A <: AnyRef](bean: A) {
+
+    def toJson(): String = TuDBJsonTool.toJson(bean)
+
+  }
+
+  def toJson(bean: Any): String = {
+    bean match {
+      case node: TuNode                 => getJson(node)
+      case relationship: TuRelationship => getJson(relationship)
+      case subPath: PathTriple          => getJson(subPath)
+      case path: LynxPath               => getJson(path)
+      case seq: Seq[Any]                => objectMapper.writeValueAsString(seq.map(toJson))
+      case m: Map[Any, Any] =>
+        objectMapper.writeValueAsString(m.map(kv => (toJson(kv._1), toJson(kv._2))))
+      case v: Any => objectMapper.writeValueAsString(v)
+    }
+  }
+
+  def getJson(node: TuNode): String = {
+    """{"identity":""" + node.id.value + ""","labels":""" + objectMapper.writeValueAsString(
+      node.labels.map(_.value)
+    ) + ""","properties":""" + objectMapper.writeValueAsString(
+      node.properties.map(kv => kv._1 -> kv._2.value)
+    ) + """}"""
+  }
+
+  def getJson(relationship: TuRelationship): String = {
+    """{"identity":""" + relationship.id.value + ""","start":""" + relationship.startId + ""","end":""" +
+      relationship.endId + ""","type":"""" + relationship.relationType.get.value +
+      """","properties":""" + objectMapper.writeValueAsString(
+        relationship.properties.map(kv => kv._1 -> kv._2.value)
+      ) + """}"""
+  }
+  def getJson(subPath: PathTriple): String = {
+    """{"start":""" + getJson(subPath.startNode.asInstanceOf[TuNode]) + ""","end":""" +
+      getJson(subPath.endNode.asInstanceOf[TuNode]) + ""","relationship":""" + getJson(
+        subPath.storedRelation.asInstanceOf[TuRelationship]
+      ) + """}"""
+  }
+
+  def getJson(path: LynxPath): String = {
+
+    """{"start":""" + getJson(path.startNode().asInstanceOf[TuNode]) + ""","end":""" + getJson(
+      path.endNode().asInstanceOf[TuNode]
+    ) +
+      ""","segments":""" + path.path.map(v => getJson(v)) +
+      """}"""
+  }
+
+}
---
+++ server/src/test/scala/org.grapheco/tudb/test/server/JsonTest.scala
@@ -0,0 +1,45 @@
+/** Copyright (c) 2022 PandaDB * */
+package org.grapheco.tudb.test.server
+
+import org.grapheco.lynx.types.LynxValue
+import org.grapheco.lynx.types.property.LynxString
+import org.grapheco.lynx.types.structural.{LynxNodeLabel, LynxRelationshipType}
+import org.grapheco.tudb.graph.{TuNode, TuRelationship}
+import org.junit._
+import org.junit.runners.MethodSorters
+
+/** @Author: huanglin
+ * @Description:
+ * @Date: Created at 2022-6-29
+ */
+@FixMethodOrder(MethodSorters.NAME_ASCENDING)
+class JsonTest {
+
+  @Test
+  def testQueryReturnJson(): Unit = {
+    import org.grapheco.tudb.TuDBJsonTool.AnyRefAddMethod
+
+    val node=TuNode(1L,List[LynxNodeLabel](LynxNodeLabel("name")),
+      List[(String,LynxValue)](("name")->LynxString("sd")))
+    val json=node.toJson()
+    println(json)
+    Assert.assertTrue(json=="""{"identity":1,"labels":["name"],"properties":{"name":"sd"}}""")
+    val relation=TuRelationship(5L,1L,2L,Some(LynxRelationshipType("a")),List(("year")->LynxString("2200")))
+    val json2=relation.toJson()
+    println(json2)
+    Assert.assertTrue(json2=="""{"identity":5,"start":1,"end":2,"type":"a","properties":{"year":"2200"}}""")
+  }
+
+
+  @Before
+  def cleanUp(): Unit = {
+
+  }
+
+  @After
+  def close(): Unit = {
+
+  }
+
+
+}
