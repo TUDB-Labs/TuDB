9676e896ee08ba35dd6be3422c77bd1aea8f2202
liam gao <503773453@qq.com>
2022-07-04 14:52:11 +0800

    test demo of ldbc cypher (#119)

---
+++ query-engine/src/test/scala/org/grapheco/tudb/ldbctest/ShortQueryTest.scala
@@ -0,0 +1,40 @@
+package org.grapheco.tudb.ldbctest
+import org.apache.commons.io.FileUtils
+import org.grapheco.tudb.ldbctest.ShortQueryTest.db
+import org.grapheco.tudb.{GraphDatabaseBuilder, TuDBInstanceContext}
+import org.grapheco.tudb.test.TestUtils
+import org.junit.{After, AfterClass, Assert, Test}
+
+import java.io.File
+
+/**
+  *@author:John117
+  *@createDate:2022/7/4
+  *@description:
+  */
+object ShortQueryTest {
+  val outputPath: String = s"${TestUtils.getModuleRootPath}/facadeTest"
+  val file = new File(outputPath)
+  if (file.exists()) FileUtils.deleteDirectory(file)
+  TuDBInstanceContext.setDataPath(outputPath)
+  val db =
+    GraphDatabaseBuilder.newEmbeddedDatabase(
+      TuDBInstanceContext.getDataPath,
+      "tudb://index?type=dummy"
+    )
+
+  @AfterClass
+  def onClose(): Unit = {
+    db.close()
+    if (file.exists()) FileUtils.deleteDirectory(file)
+  }
+}
+
+class ShortQueryTest {
+  @After
+  def clean(): Unit = {
+    db.cypher("match (n) detach delete n")
+  }
+  @Test
+  def Q1(): Unit = {}
+}
