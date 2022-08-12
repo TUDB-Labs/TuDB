ad3756029e7adc0e45551ed6514565c2051f2129
LianxinGao <503773453@qq.com>
2022-08-12 11:09:09 +0800

    update code

--- lynx/src/main/scala/org/grapheco/lynx/operator/LimitOperator.scala
+++ lynx/src/main/scala/org/grapheco/lynx/operator/LimitOperator.scala
@@ -1,7 +1,9 @@
 package org.grapheco.lynx.operator
 
 import org.grapheco.lynx.types.LynxValue
+import org.grapheco.lynx.types.property.LynxNumber
 import org.grapheco.lynx.{ExecutionOperator, ExpressionContext, ExpressionEvaluator, LynxType, RowBatch}
+import org.grapheco.tudb.exception.{TuDBError, TuDBException}
 import org.opencypher.v9_0.expressions.Expression
 
 import scala.collection.mutable.ArrayBuffer
@@ -23,7 +25,24 @@ case class LimitOperator(
 
   override def openImpl(): Unit = {
     in.open()
-    limitNum = evalExpr(limitExpr)(exprContext).value.asInstanceOf[Number].intValue()
+
+    val evalValue = evalExpr(limitExpr)(exprContext)
+    evalValue match {
+      case n: LynxNumber => {
+        val num = n.value.asInstanceOf[Number].intValue()
+        if (num >= 0) limitNum = num
+        else
+          throw new TuDBException(
+            TuDBError.LYNX_WRONG_NUMBER_OF_ARGUMENT,
+            s"Invalid input '$num', must be a positive integer."
+          )
+      }
+      case n =>
+        throw new TuDBException(
+          TuDBError.LYNX_WRONG_NUMBER_OF_ARGUMENT,
+          s"Invalid input '${n.value}', must be a positive integer."
+        )
+    }
   }
 
   override def getNextImpl(): RowBatch = {
