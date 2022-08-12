1b691ddfb83ba84dc39e0a44ecdce663a47d387b
LianxinGao <503773453@qq.com>
2022-08-12 11:06:45 +0800

    update code

--- lynx/src/main/scala/org/grapheco/lynx/operator/SkipOperator.scala
+++ lynx/src/main/scala/org/grapheco/lynx/operator/SkipOperator.scala
@@ -1,7 +1,9 @@
 package org.grapheco.lynx.operator
 
 import org.grapheco.lynx.types.LynxValue
+import org.grapheco.lynx.types.property.LynxNumber
 import org.grapheco.lynx.{ExecutionOperator, ExpressionContext, ExpressionEvaluator, LynxType, RowBatch}
+import org.grapheco.tudb.exception.{TuDBError, TuDBException}
 import org.opencypher.v9_0.expressions.Expression
 
 import scala.collection.mutable.ArrayBuffer
@@ -24,7 +26,23 @@ case class SkipOperator(
 
   override def openImpl(): Unit = {
     in.open()
-    skipLength = evalExpr(skipExpr)(exprContext).value.asInstanceOf[Number].intValue()
+    val evalValue = evalExpr(skipExpr)(exprContext)
+    evalValue match {
+      case n: LynxNumber => {
+        val num = n.value.asInstanceOf[Number].intValue()
+        if (num >= 0) skipLength = num
+        else
+          throw new TuDBException(
+            TuDBError.LYNX_WRONG_NUMBER_OF_ARGUMENT,
+            s"Invalid input '$num', must be a positive integer."
+          )
+      }
+      case n =>
+        throw new TuDBException(
+          TuDBError.LYNX_WRONG_NUMBER_OF_ARGUMENT,
+          s"Invalid input '${n.value}', must be a positive integer."
+        )
+    }
   }
 
   override def getNextImpl(): RowBatch = {
