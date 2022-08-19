310dfe51a99b02b3e9e6369449c9bab69905c41d
Yuan Tang <terrytangyuan@gmail.com>
2022-08-19 14:03:25 -0400

    relationship client code
    Signed-off-by: Yuan Tang <terrytangyuan@gmail.com>

--- client/src/main/scala/org/grapheco/tudb/client/GraphAPIClient.scala
+++ client/src/main/scala/org/grapheco/tudb/client/GraphAPIClient.scala
@@ -3,7 +3,7 @@ package org.grapheco.tudb.client
 import com.typesafe.scalalogging.LazyLogging
 import io.grpc.ManagedChannel
 import io.grpc.netty.shaded.io.grpc.netty.NettyChannelBuilder
-import org.grapheco.tudb.core.{Core, NodeServiceGrpc}
+import org.grapheco.tudb.core.{Core, NodeServiceGrpc, RelationshipServiceGrpc}
 import org.slf4j.LoggerFactory
 
 import java.util.concurrent.TimeUnit
@@ -38,6 +38,20 @@ class GraphAPIClient(host: String, port: Int) extends LazyLogging {
     }
   }
 
+  def createRelationship(relationship: Core.Relationship): Core.Relationship = {
+    val request: Core.RelationshipCreateRequest =
+      Core.RelationshipCreateRequest.newBuilder().setRelationship(relationship).build()
+    val response = relationshipServiceBlockingStub.createRelationship(request)
+    if (response.getStatus.getExitCode == 0) {
+      response.getRelationship
+    } else {
+      logger.info(
+        f"Failed to create relationship ${relationship.getName}: ${response.getStatus.getMessage}"
+      )
+      null
+    }
+  }
+
   def getNode(id: Long): Core.Node = {
     val request: Core.NodeGetRequest =
       Core.NodeGetRequest.newBuilder().setNodeId(id).build()
@@ -47,6 +61,19 @@ class GraphAPIClient(host: String, port: Int) extends LazyLogging {
       response.getNode
     } else {
       logger.info(f"Failed to get node $id: ${response.getStatus.getMessage}")
+      null
+    }
+  }
+
+  def getRelationship(id: Long): Core.Relationship = {
+    val request: Core.RelationshipGetRequest =
+      Core.RelationshipGetRequest.newBuilder().setRelationshipId(id).build()
+    val response = relationshipServiceBlockingStub.getRelationship(request)
+    // TODO: Need to check whether the node is null. response.hasNode
+    if (response.getStatus.getExitCode == 0) {
+      response.getRelationship
+    } else {
+      logger.info(f"Failed to get relationship $id: ${response.getStatus.getMessage}")
       null
     }
   }
@@ -62,6 +89,17 @@ class GraphAPIClient(host: String, port: Int) extends LazyLogging {
     }
   }
 
+  def deleteRelationship(id: Long) {
+    val request: Core.RelationshipDeleteRequest =
+      Core.RelationshipDeleteRequest.newBuilder().setRelationshipId(id).build()
+    val response = relationshipServiceBlockingStub.deleteRelationship(request)
+    if (response.getStatus.getExitCode == 0) {
+      logger.info(f"Successfully deleted relationship $id")
+    } else {
+      logger.info(f"Failed to delete relationship $id: ${response.getStatus.getMessage}")
+    }
+  }
+
   def listNodes(): List[Core.Node] = {
     val request: Core.NodeListRequest =
       Core.NodeListRequest.newBuilder().build()
@@ -70,6 +108,18 @@ class GraphAPIClient(host: String, port: Int) extends LazyLogging {
       response.getNodesList.asScala.toList
     } else {
       logger.info(f"Failed to list nodes")
+      null
+    }
+  }
+
+  def listRelationship(): List[Core.Relationship] = {
+    val request: Core.RelationshipListRequest =
+      Core.RelationshipListRequest.newBuilder().build()
+    val response = relationshipServiceBlockingStub.listRelationships(request)
+    if (response.getStatus.getExitCode == 0) {
+      response.getRelationshipsList.asScala.toList
+    } else {
+      logger.info(f"Failed to list relationships")
       null
     }
   }
