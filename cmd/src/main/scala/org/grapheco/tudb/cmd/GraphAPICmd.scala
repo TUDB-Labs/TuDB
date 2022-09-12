e37612602f5bd0d24abdd353b7371342e1fc791a
Yuan Tang <terrytangyuan@gmail.com>
2022-09-12 10:00:03 -0400

    wip
    Signed-off-by: Yuan Tang <terrytangyuan@gmail.com>

--- cmd/src/main/scala/org/grapheco/tudb/cmd/GraphAPICmd.scala
+++ cmd/src/main/scala/org/grapheco/tudb/cmd/GraphAPICmd.scala
@@ -6,4 +6,24 @@ import org.slf4j.LoggerFactory
 class GraphAPICmd() extends LazyLogging {
 
   val LOGGER = LoggerFactory.getLogger("graph-api-cmd-info")
+  // Class variables
+  var number: Int = 16
+  var nameofcompany: String = "Apple"
+
+  // Class method
+  def Display()
+  {
+    println("Name of the company : " + nameofcompany);
+    println("Total number of Smartphone generation: " + number);
+  }
+}
+
+object Main {
+  def main(args: Array[String])
+  {
+
+    // Class object
+    var obj = new GraphAPICmd();
+    obj.Display();
+  }
 }
