8c97527de44b445d9ea3684011d0ebc91f6dfcf5
LianxinGao <503773453@qq.com>
2022-10-07 13:33:18 +0800

    update listLiteral

--- lynx/src/main/scala/org/grapheco/lynx/expression/LynxListLiteral.scala
+++ lynx/src/main/scala/org/grapheco/lynx/expression/LynxListLiteral.scala
@@ -3,4 +3,14 @@ package org.grapheco.lynx.expression
 /**
   *@description:
   */
-case class LynxListLiteral(expressions: Seq[LynxExpression]) extends LynxExpression {}
+case class LynxListLiteral(expressions: Seq[LynxExpression]) extends LynxExpression {
+  def map(f: LynxExpression => LynxExpression) = copy(expressions = expressions.map(f))
+
+  override def asCanonicalStringVal: String =
+    expressions.map(_.asCanonicalStringVal).mkString("[", ", ", "]")
+}
+
+case class ListSlice(list: LynxExpression, from: Option[LynxExpression], to: Option[LynxExpression])
+  extends LynxExpression
+
+case class ContainerIndex(expr: LynxExpression, idx: LynxExpression) extends LynxExpression
